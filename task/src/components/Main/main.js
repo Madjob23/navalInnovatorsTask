3b4f791ab486ce689ba1455639cfb6b3b8fe0096
madhav <smadhvendra20@gmail.com>
2024-12-17 21:11:41 +0530

    Define and style navbar

---
+++ task/src/components/Main/main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./main.css";
+
+const Main = () => {
+    
+  return (
+    <div className='main'>
+      <nav>
+        <ul className="tabs">
+            <li className="tab">Syllabus</li>
+            <li className="tab">Q&A</li>
+            <li className="tab">Unit</li>
+            <li className="tab">Insights</li>
+            <li className="tab">Maps</li>
+            <li className="tab">Custom Preparation</li>
+            <li className="tab">AI Featured</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Main
