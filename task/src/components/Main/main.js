3150d13f6a7a26c6d1d820f94dffed7feb499bcf
madhav <smadhvendra20@gmail.com>
2024-12-17 21:16:17 +0530

    Change tab's color on click

--- task/src/components/Main/main.js
+++ task/src/components/Main/main.js
@@ -2,18 +2,26 @@ import React from 'react';
 import "./main.css";
 
 const Main = () => {
+
+  const handleTabClick = (e) => {
+    const anyClicked = document.querySelector('.clickedTab');
+    if (anyClicked) {
+        anyClicked.classList.remove('clickedTab');
+    }
+    e.target.classList.add('clickedTab');
+  }
     
   return (
     <div className='main'>
       <nav>
         <ul className="tabs">
-            <li className="tab">Syllabus</li>
-            <li className="tab">Q&A</li>
-            <li className="tab">Unit</li>
-            <li className="tab">Insights</li>
-            <li className="tab">Maps</li>
-            <li className="tab">Custom Preparation</li>
-            <li className="tab">AI Featured</li>
+            <li onClick={handleTabClick} className="tab">Syllabus</li>
+            <li onClick={handleTabClick} className="tab">Q&A</li>
+            <li onClick={handleTabClick} className="tab">Unit</li>
+            <li onClick={handleTabClick} className="tab">Insights</li>
+            <li onClick={handleTabClick} className="tab">Maps</li>
+            <li onClick={handleTabClick} className="tab">Custom Preparation</li>
+            <li onClick={handleTabClick} className="tab">AI Featured</li>
         </ul>
       </nav>
     </div>
