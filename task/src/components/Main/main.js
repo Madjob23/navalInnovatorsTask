8cf18642bc2edc64492390a84a62dd60b00ce77c
madhav <smadhvendra20@gmail.com>
2024-12-17 23:45:59 +0530

    Import question component

--- task/src/components/Main/main.js
+++ task/src/components/Main/main.js
@@ -1,11 +1,13 @@
 import "./main.css";
 import React from 'react';
 import Navbar from "../Navbar/navbar";
+import Question from "../Question/question";
 
 const Main = () => {
   return (
     <div className="main">
         <Navbar />
+        <Question />
     </div>
   )
 }
