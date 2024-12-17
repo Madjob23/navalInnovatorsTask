aef2bb24903faa6d4a7d35d22094b48a2bfa3ec1
madhav <smadhvendra20@gmail.com>
2024-12-17 18:43:35 +0530

    Import sidebar and main in a section

--- task/src/App.js
+++ task/src/App.js
@@ -1,11 +1,18 @@
+import "./app.css";
 import Header from "./components/Header/header";
 import ProgressBar from "./components/ProgressBar/progressBar";
+import Sidebar from "./components/Sidebar/sidebar";
+import Main from "./components/Main/main";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <ProgressBar />
+      <section className="content">
+        <Sidebar />
+        <Main />
+      </section>
     </div>
   );
 }
