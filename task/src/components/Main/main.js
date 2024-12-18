7cd17f17a7fb52f27765fbedc3b3a3207919b40d
madhav <smadhvendra20@gmail.com>
2024-12-18 16:36:30 +0530

    Import lower page nav

--- task/src/components/Main/main.js
+++ task/src/components/Main/main.js
@@ -2,12 +2,14 @@ import "./main.css";
 import React from 'react';
 import Navbar from "../Navbar/navbar";
 import Question from "../Question/question";
+import Pagenav from "../Pagenav/pagenav";
 
 const Main = () => {
   return (
     <div className="main">
         <Navbar />
         <Question />
+        <Pagenav />
     </div>
   )
 }
