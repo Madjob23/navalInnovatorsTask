a905db579a8972c3b18ce75cc36088d1bc781d25
madhav <smadhvendra20@gmail.com>
2024-12-18 17:33:33 +0530

    Import component

--- task/src/components/Sidebar/sidebar.js
+++ task/src/components/Sidebar/sidebar.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import "./sidebar.css";
+import Unitselector from '../UnitSelector/unitselector';
+import Queslist from '../Queslist/queslist';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-      <select name="unitSelector" id="unitSelector">
-        <option value="null" selected>Select unit</option>
-        <option value="unit1">Unit 1 <span id="unitCompletion"></span></option>
-        <option value="unit2">Unit 2 <span id="unitCompletion"></span></option>
-        <option value="unit3">Unit 3 <span id="unitCompletion"></span></option>
-        <option value="unit4">Unit 4 <span id="unitCompletion"></span></option>
-        <option value="unit5">Unit 5 <span id="unitCompletion"></span></option>
-      </select>
+      <Unitselector />
+      <Queslist />
     </div>
   )
 }
